a3c4b779cfa0a93af7a3a9ae6ebd7b9cec884267
Chriskatalon <quan.trinh@katalon.com>
2023-05-01 15:53:22 +0700

    commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6a0b7e0a-37b2-457b-bd1c-dc7bffd9fdca</testSuiteGuid>
+</TestSuiteEntity>
