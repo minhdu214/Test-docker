9a60a43c4cc049f439120ea9d197077c9d98fa27
Chriskatalon <quan.trinh@katalon.com>
2023-05-01 16:22:47 +0700

    commit

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>6a0b7e0a-37b2-457b-bd1c-dc7bffd9fdca</testSuiteGuid>
+   <testCaseLink>
+      <guid>947219db-1d3b-46c8-88a5-7bc37403ac77</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/KatalonTest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
