51bc253d2d5d17a6a581f20cac12c868eb3a2723
Chriskatalon <quan.trinh@katalon.com>
2023-05-02 17:06:33 +0700

    commit

--- Scripts/KatalonTest/Script1682690119744.groovy
+++ Scripts/KatalonTest/Script1682690119744.groovy
@@ -1,19 +1,27 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.chrome.ChromeDriver
-
-import com.kms.katalon.core.webui.driver.DriverFactory
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.chrome.ChromeDriver as ChromeDriver
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import internal.GlobalVariable as GlobalVariable
 
-String chromedriverLog = System.getProperty("user.dir") + "/chromedriver.log"
-println chromedriverLog
-System.setProperty("webdriver.chrome.logfile", chromedriverLog);
-System.setProperty("webdriver.chrome.verboseLogging", "true");
-System.setProperty('webdriver.chrome.driver', 'PATH\\chromedriver.exe')
-WebDriver driver = new ChromeDriver()
-DriverFactory.changeWebDriver(driver)
-driver.get('https://www.google.com')
+WebUI.openBrowser(null)
+
+WebUI.navigateToUrl('https://uat.ccm.tutenlabs.dev/')
+
 WebUI.delay(30)
 
 WebUI.setViewPortSize(1920, 1080)
@@ -29,6 +37,8 @@ WebUI.click(findTestObject('Object Repository/Page_Tutenlabs/span_Iniciar sesin'
 WebUI.click(findTestObject('Object Repository/Page_Tutenlabs/span_Suscripciones'))
 
 WebUI.click(findTestObject('Object Repository/Page_Tutenlabs/svg_Control_MuiSvgIcon-root'))
+
+WebUI.delay(5)
 
 WebUI.click(findTestObject('Object Repository/Page_Tutenlabs/span_Administracin'))
 
